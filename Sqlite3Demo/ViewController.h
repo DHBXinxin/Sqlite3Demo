b605742099e1d6c36c598332aea55cec018d083b
李如欣 <461132172@qq.com>
2016-06-17 11:48:35 +0800

    Initial Commit

---
+++ Sqlite3Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sqlite3Demo
+//
+//  Created by Shorigo on 16/6/17.
+//  Copyright © 2016年 Shorigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sqlite3Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Sqlite3Demo
+//
+//  Created by Shorigo on 16/6/17.
+//  Copyright © 2016年 Shorigo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
